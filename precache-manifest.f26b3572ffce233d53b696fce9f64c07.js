a82a77b01490309617866052e1da8e995baf9cde
Matt Donaldson <matthewdonaldson9@gmail.com>
2019-09-09 17:26:01 -0400

    Updates

---
+++ precache-manifest.f26b3572ffce233d53b696fce9f64c07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d7fbc2ce86bafc78df978743989e49",
+    "url": "/projectFiveRecipeAppMattDonaldson/index.html"
+  },
+  {
+    "revision": "41bd717c996f2599688d",
+    "url": "/projectFiveRecipeAppMattDonaldson/static/css/main.de22d64a.chunk.css"
+  },
+  {
+    "revision": "8f4884bb2f422970fba6",
+    "url": "/projectFiveRecipeAppMattDonaldson/static/js/2.402a6276.chunk.js"
+  },
+  {
+    "revision": "41bd717c996f2599688d",
+    "url": "/projectFiveRecipeAppMattDonaldson/static/js/main.2a0dea48.chunk.js"
+  },
+  {
+    "revision": "a22c71b225884907494b",
+    "url": "/projectFiveRecipeAppMattDonaldson/static/js/runtime~main.a3b0e94b.js"
+  },
+  {
+    "revision": "29777881c3ef1bb1c470119a1081a4fb",
+    "url": "/projectFiveRecipeAppMattDonaldson/static/media/heroImage1.29777881.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.798932332b77b2e5bbea2e30018d54d5.js"
+  "/projectFiveRecipeAppMattDonaldson/precache-manifest.f26b3572ffce233d53b696fce9f64c07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projectFiveRecipeAppMattDonaldson/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
